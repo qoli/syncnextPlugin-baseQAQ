06094f496243c23735b327b34fc715c7d055c6f8
Ronnie W. <llqoli@gmail.com>
2024-09-12 15:38:06 +0800

    avfuns 搜尋 api 更新

--- alpha_v2/anfuns.js
+++ alpha_v2/anfuns.js
@@ -1,154 +1,151 @@
 `user script`;
 
 function buildMediaData(
-    id,
-    coverURLString,
-    title,
-    descriptionText,
-    detailURLString
+  id,
+  coverURLString,
+  title,
+  descriptionText,
+  detailURLString
 ) {
-    var obj = {
-        id: id,
-        coverURLString: coverURLString,
-        title: title,
-        descriptionText: descriptionText,
-        detailURLString: detailURLString,
-    };
+  var obj = {
+    id: id,
+    coverURLString: coverURLString,
+    title: title,
+    descriptionText: descriptionText,
+    detailURLString: detailURLString,
+  };
 
-    return obj;
+  return obj;
 }
 function buildEpisodeData(id, title, episodeDetailURL) {
-    return {
-        id: id,
-        title: title,
-        episodeDetailURL: episodeDetailURL,
-    };
+  return {
+    id: id,
+    title: title,
+    episodeDetailURL: episodeDetailURL,
+  };
 }
 // Main
 
 function buildMedias(inputURL) {
-    var req = {
-        url: inputURL,
-        method: "GET",
-    };
+  var req = {
+    url: inputURL,
+    method: "GET",
+  };
 
-    let datas = [];
+  let datas = [];
 
-    $http.fetch(req).then(function (res) {
-        var content = tXml.getElementsByClassName(res.body, "hl-list-item");
+  $http.fetch(req).then(function (res) {
+    var content = tXml.getElementsByClassName(res.body, "hl-list-item");
 
-        for (var index = 0; index < content.length; index++) {
+    for (var index = 0; index < content.length; index++) {
+      var dom = content[index];
+      //print(content.length)
 
-            var dom = content[index];
-            //print(content.length)
+      var title = findAllByKey(dom, "title")[0];
+      var href = findAllByKey(dom, "href")[0];
+      var coverURLString = findAllByKey(dom, "data-original")[0];
+      var descriptionText = "";
 
-            var title = findAllByKey(dom, "title")[0];
-            var href = findAllByKey(dom, "href")[0];
-            var coverURLString = findAllByKey(dom, "data-original")[0];
-            var descriptionText = '';
+      href = buildURL(href);
 
-            href = buildURL(href);
+      datas.push(
+        buildMediaData(href, coverURLString, title, descriptionText, href)
+      );
+    }
 
-            datas.push(
-                buildMediaData(href, coverURLString, title, descriptionText, href)
-            );
-        }
-
-        $next.toMedias(JSON.stringify(datas));
-    });
+    $next.toMedias(JSON.stringify(datas));
+  });
 }
 function buildURL(href) {
-    if (!href.startsWith("http")) {
-        href = "https://www.anfuns.cc" + href;
-    }
-    return href;
+  if (!href.startsWith("http")) {
+    href = "https://www.anfuns.cc" + href;
+  }
+  return href;
 }
 
 function Episodes(inputURL) {
-    var req = {
-        url: inputURL,
-        method: "GET",
-    };
+  var req = {
+    url: inputURL,
+    method: "GET",
+  };
 
-    let datas = [];
+  let datas = [];
 
-    $http.fetch(req).then(function (res) {
-        var content = tXml.getElementsByClassName(res.body, "hl-plays-list")[0];
-        //print(content.children.length);
+  $http.fetch(req).then(function (res) {
+    var content = tXml.getElementsByClassName(res.body, "hl-plays-list")[0];
+    //print(content.children.length);
 
-        for (var index = 0; index < content.children.length; index++) {
-            var element = content.children[index].children[0];
-            //print(element);
+    for (var index = 0; index < content.children.length; index++) {
+      var element = content.children[index].children[0];
+      //print(element);
 
-            var href = element.attributes ? element.attributes.href : '';
-            var title = element.children.toString();
+      var href = element.attributes ? element.attributes.href : "";
+      var title = element.children.toString();
 
-            href = buildURL(href);
+      href = buildURL(href);
 
-            datas.push(buildEpisodeData(href, title, href));
-        }
+      datas.push(buildEpisodeData(href, title, href));
+    }
 
-        $next.toEpisodes(JSON.stringify(datas));
-    });
+    $next.toEpisodes(JSON.stringify(datas));
+  });
 }
 function Player(inputURL) {
-    var req = {
-        url: inputURL,
-        method: "GET",
-    };
+  var req = {
+    url: inputURL,
+    method: "GET",
+  };
 
-    $http.fetch(req).then(function (res) {
-        var xml = res.body;
-        html = xml.match(/r player_.*?=(.*?)</)[1];
+  $http.fetch(req).then(function (res) {
+    var xml = res.body;
+    html = xml.match(/r player_.*?=(.*?)</)[1];
 
-        var js = JSON.parse(html);
-        urlEncode = base64Decode(js.url)
-        url = decodeURIComponent(urlEncode)
-        $next.toPlayer(url);
-
-    })
+    var js = JSON.parse(html);
+    urlEncode = base64Decode(js.url);
+    url = decodeURIComponent(urlEncode);
+    $next.toPlayer(url);
+  });
 }
 
-function Search(inputURL) {
-    const req = {
-        //url: "https://www.histar.tv/_next/data/" + buildId + "/search.json?word=" + inputURL,
-        url: inputURL,//直接從網頁獲取搜索結果
-        method: "GET",
-    };
-    $http.fetch(req).then(res => {
-        var content = tXml.getElementsByClassName(res.body, "hl-item-div");
+function Search(inputURL, key) {
+  const req = {
+    //url: "https://www.histar.tv/_next/data/" + buildId + "/search.json?word=" + inputURL,
+    url: inputURL, //直接從網頁獲取搜索結果
+    method: "GET",
+  };
+  $http.fetch(req).then((res) => {
+    var content = tXml.getElementsByClassName(res.body, "hl-item-div");
 
-        let datas = [];
-        for (var index = 0; index < content.length; index++) {
-            var dom = content[index];
-            //print(content.length)
+    let datas = [];
+    for (var index = 0; index < content.length; index++) {
+      var dom = content[index];
+      //print(content.length)
 
-            var title = findAllByKey(dom, "title")[0];
-            var href = findAllByKey(dom, "href")[0];
+      var title = findAllByKey(dom, "title")[0];
+      var href = findAllByKey(dom, "href")[0];
 
-            href = buildURL(href);
+      href = buildURL(href);
 
-            datas.push(buildEpisodeData(href, title, href));
-        }
+      datas.push(buildEpisodeData(href, title, href));
+    }
 
-        $next.toMedias(JSON.stringify(datas));
-    });
+    $next.toSearchMedias(JSON.stringify(datas), key);
+  });
 }
-
 
 function findAllByKey(obj, keyToFind) {
-    return (
-        Object.entries(obj).reduce(
-            (acc, [key, value]) =>
-                key === keyToFind
-                    ? acc.concat(value)
-                    : typeof value === "object" && value
-                        ? acc.concat(findAllByKey(value, keyToFind))
-                        : acc,
-            []
-        ) || []
-    );
+  return (
+    Object.entries(obj).reduce(
+      (acc, [key, value]) =>
+        key === keyToFind
+          ? acc.concat(value)
+          : typeof value === "object" && value
+          ? acc.concat(findAllByKey(value, keyToFind))
+          : acc,
+      []
+    ) || []
+  );
 }
 function print(params) {
-    console.log(JSON.stringify(params));
+  console.log(JSON.stringify(params));
 }
