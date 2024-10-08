c6b0ac36015cfe34bcb9e00607f15d92981436a2
Ronnie W. <llqoli@gmail.com>
2024-10-08 19:14:20 +0800

    增加 grigrilove 的搜尋支持

---
+++ test.js
@@ -0,0 +1,357 @@
+var dom = `
+{
+  "tagName": "div",
+  "attributes": {
+    "class": "public-list-box search-box flex rel"
+  },
+  "children": [
+    {
+      "tagName": "div",
+      "attributes": {
+        "class": "cover",
+        "style": "background-image: linear-gradient(180deg, rgb(17, 19, 25) 0%, rgba(25,26,33,0.90) 52%, rgb(17, 19, 25) 100%), url(/upload/vod/20240608-1/d31a7854cc5dd15b1903ba84c8a2b53e.webp);"
+      },
+      "children": []
+    },
+    {
+      "tagName": "div",
+      "attributes": {
+        "class": "left public-list-bj"
+      },
+      "children": [
+        {
+          "tagName": "a",
+          "attributes": {
+            "target": "_blank",
+            "class": "public-list-exp",
+            "href": "/GV25964/"
+          },
+          "children": [
+            {
+              "tagName": "img",
+              "attributes": {
+                "referrerpolicy": "no-referrer",
+                "src": "data:image/gif;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVQImWNgYGBgAAAABQABh6FO1AAAAABJRU5ErkJggg==",
+                "class": "lazy lazy1 gen-movie-img mask-1",
+                "alt": "蓝色巨人封面图",
+                "data-src": "/upload/vod/20240608-1/d31a7854cc5dd15b1903ba84c8a2b53e.webp"
+              },
+              "children": []
+            },
+            {
+              "tagName": "span",
+              "attributes": {
+                "class": "public-list-prb hide ft2"
+              },
+              "children": [
+                "支持杜比视界 建议佩戴耳机"
+              ]
+            },
+            {
+              "tagName": "span",
+              "attributes": {
+                "class": "public-bg"
+              },
+              "children": []
+            },
+            {
+              "tagName": "span",
+              "attributes": {
+                "class": "public-play"
+              },
+              "children": [
+                {
+                  "tagName": "i",
+                  "attributes": {
+                    "class": "fa"
+                  },
+                  "children": [
+                    "&#xe593;"
+                  ]
+                }
+              ]
+            }
+          ]
+        }
+      ]
+    },
+    {
+      "tagName": "div",
+      "attributes": {
+        "class": "right rel flex-auto"
+      },
+      "children": [
+        {
+          "tagName": "div",
+          "attributes": {
+            "class": "thumb-content"
+          },
+          "children": [
+            {
+              "tagName": "div",
+              "attributes": {
+                "class": "thumb-txt cor4 hide"
+              },
+              "children": [
+                "蓝色巨人"
+              ]
+            },
+            {
+              "tagName": "div",
+              "attributes": {
+                "class": "thumb-else cor5 hide"
+              },
+              "children": [
+                {
+                  "tagName": "span",
+                  "attributes": {},
+                  "children": [
+                    {
+                      "tagName": "a",
+                      "attributes": {
+                        "href": "/search/-------------2023/",
+                        "target": "_blank"
+                      },
+                      "children": [
+                        "2023"
+                      ]
+                    },
+                    "&nbsp;"
+                  ]
+                },
+                {
+                  "tagName": "span",
+                  "attributes": {},
+                  "children": [
+                    {
+                      "tagName": "a",
+                      "attributes": {
+                        "href": "/search/--%E6%97%A5%E6%9C%AC-----------/",
+                        "target": "_blank"
+                      },
+                      "children": [
+                        "日本"
+                      ]
+                    },
+                    "&nbsp;"
+                  ]
+                },
+                {
+                  "tagName": "span",
+                  "attributes": {},
+                  "children": [
+                    {
+                      "tagName": "a",
+                      "attributes": {
+                        "href": "/search/----%E9%9F%B3%E4%B9%90---------/",
+                        "target": "_blank"
+                      },
+                      "children": [
+                        "音乐"
+                      ]
+                    },
+                    "&nbsp;",
+                    {
+                      "tagName": "a",
+                      "attributes": {
+                        "href": "/search/----%E7%83%AD%E8%A1%80---------/",
+                        "target": "_blank"
+                      },
+                      "children": [
+                        "热血"
+                      ]
+                    },
+                    "&nbsp;"
+                  ]
+                }
+              ]
+            },
+            {
+              "tagName": "div",
+              "attributes": {
+                "class": "thumb-director cor5 hide"
+              },
+              "children": [
+                {
+                  "tagName": "a",
+                  "attributes": {
+                    "class": "b",
+                    "href": "javascript:"
+                  },
+                  "children": [
+                    "导演："
+                  ]
+                },
+                {
+                  "tagName": "a",
+                  "attributes": {
+                    "href": "/search/-----%E7%AB%8B%E5%B7%9D%E8%AE%A9--------/",
+                    "target": "_blank"
+                  },
+                  "children": [
+                    "立川让"
+                  ]
+                },
+                {
+                  "tagName": "em",
+                  "attributes": {},
+                  "children": [
+                    "/"
+                  ]
+                }
+              ]
+            },
+            {
+              "tagName": "div",
+              "attributes": {
+                "class": "thumb-actor cor5 hide"
+              },
+              "children": [
+                {
+                  "tagName": "a",
+                  "attributes": {
+                    "class": "b",
+                    "href": "javascript:"
+                  },
+                  "children": [
+                    "主演："
+                  ]
+                },
+                {
+                  "tagName": "a",
+                  "attributes": {
+                    "href": "/search/-%E5%B1%B1%E7%94%B0%E8%A3%95%E8%B4%B5------------/",
+                    "target": "_blank"
+                  },
+                  "children": [
+                    "山田裕贵"
+                  ]
+                },
+                {
+                  "tagName": "em",
+                  "attributes": {},
+                  "children": [
+                    "/"
+                  ]
+                },
+                {
+                  "tagName": "a",
+                  "attributes": {
+                    "href": "/search/-%E9%97%B4%E5%AE%AB%E7%A5%A5%E5%A4%AA%E6%9C%97------------/",
+                    "target": "_blank"
+                  },
+                  "children": [
+                    "间宫祥太朗"
+                  ]
+                },
+                {
+                  "tagName": "em",
+                  "attributes": {},
+                  "children": [
+                    "/"
+                  ]
+                },
+                {
+                  "tagName": "a",
+                  "attributes": {
+                    "href": "/search/-%E5%86%88%E5%B1%B1%E5%A4%A9%E9%9F%B3------------/",
+                    "target": "_blank"
+                  },
+                  "children": [
+                    "冈山天音"
+                  ]
+                },
+                {
+                  "tagName": "em",
+                  "attributes": {},
+                  "children": [
+                    "/"
+                  ]
+                }
+              ]
+            }
+          ]
+        },
+        {
+          "tagName": "span",
+          "attributes": {
+            "class": "cor5 thumb-blurb hide2"
+          },
+          "children": [
+            "住在仙台的高中生・宫本大（山田裕贵 配音）深受爵士乐吸引，每天都独自在河边吹奏次中音萨克斯风。毕业后他来到东京，住进了高中同学・玉田俊二（冈山天音 配音）的公寓。某日，大在展演空间遇见同龄钢琴家・泽边雪祈（间宫祥太朗 配音），并邀请他共组乐团，受大影响而开始打鼓的玉田也加入组成3 人乐团“JASS”。连乐谱都看不懂，只是一心吹奏萨克斯风的大、从小就将一切献给爵士乐的雪祈、以及初学者玉田，以在日本最好的爵士乐具乐部表演并改变日本爵士乐风格为目标，尽全力投入在音乐活动之中。"
+          ]
+        },
+        {
+          "tagName": "div",
+          "attributes": {
+            "class": "thumb-menu flex"
+          },
+          "children": [
+            {
+              "tagName": "a",
+              "attributes": {
+                "target": "_blank",
+                "href": "/GV25964/",
+                "class": "button cr8"
+              },
+              "children": [
+                {
+                  "tagName": "i",
+                  "attributes": {
+                    "class": "fa r3 ds-bofang1"
+                  },
+                  "children": []
+                },
+                "播放正片"
+              ]
+            },
+            {
+              "tagName": "a",
+              "attributes": {
+                "class": "thumb-s collection fa ds-shoucang",
+                "data-type": "2",
+                "data-mid": "1",
+                "data-id": "25964"
+              },
+              "children": []
+            }
+          ]
+        }
+      ]
+    }
+  ]
+}
+`;
+
+function findAllByKey(obj, keyToFind) {
+  // 如果 obj 不是对象，则尝试将其解析为 JSON 对象
+  if (typeof obj === "string") {
+    try {
+      obj = JSON.parse(obj);
+    } catch (e) {
+      console.error("提供的字符串无法解析为 JSON:", e);
+      return [];
+    }
+  }
+
+  // 如果 obj 依然不是对象，返回空数组
+  if (typeof obj !== "object" || obj === null) {
+    return [];
+  }
+
+  // 递归查找指定键
+  return Object.entries(obj).reduce((acc, [key, value]) => {
+    if (key === keyToFind) {
+      return acc.concat(value);
+    }
+
+    if (typeof value === "object") {
+      return acc.concat(findAllByKey(value, keyToFind));
+    }
+
+    return acc;
+  }, []);
+}
+
+var href = findAllByKey(dom, "children")[2]["children"]["1"]["children"][0];
+console.log(href);
