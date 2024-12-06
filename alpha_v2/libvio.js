93aff7dbf632f03d072fc61be8728cd0bfce73b7
Ronnie W. <llqoli@gmail.com>
2024-12-06 15:53:57 +0800

    Revert "更新 libvio 的 HTTP 頭"
    This reverts commit 64407cf1bf2d1bfea6aa458bbb0fac1f254ca03f.

--- alpha_v2/libvio.js
+++ alpha_v2/libvio.js
@@ -237,6 +237,8 @@ function gotoPlay(url) {
     let json = {
       url: url,
       headers: {
+        "User-Agent":
+          "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/18.1 Safari/605.1.15",
         Referer: "https://www.libvio.vip/",
       },
     };
