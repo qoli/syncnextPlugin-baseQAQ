1cc7395557ae484a630800704f90fac20317b134
Ronnie W. <llqoli@gmail.com>
2025-10-18 08:52:32 +0800

    🐛 fix(libvio): 更新 libvio 網域   - 將 libvio.pro 替換為 www.libvio.cc   - 更新基礎 URL、播放器 API 網址及 Referer 標頭   - 解決因原網域失效導致的功能中斷問題

--- alpha_v2/libvio.js
+++ alpha_v2/libvio.js
@@ -32,7 +32,7 @@ function buildEpisodeData(id, title, episodeDetailURL) {
 
 function buildURL(href) {
   if (!href.startsWith("http")) {
-    href = "https://libvio.pro" + href;
+    href = "https://www.libvio.cc" + href;
   }
   return href;
 }
@@ -131,9 +131,9 @@ function Player(inputURL) {
     var next = js.link_next;
     var id = js.id;
     var nid = js.nid;
-    var paurl = "https://libvio.pro/vid/ty3.php?url=";
+    var paurl = "https://www.libvio.cc/vid/ty3.php?url=";
     var req2 = {
-      url: "https://libvio.pro" + "/static/player/" + from + ".js",
+      url: "https://www.libvio.cc" + "/static/player/" + from + ".js",
       method: "GET",
     };
 
@@ -147,13 +147,13 @@ function Player(inputURL) {
       case "ty_new1":
         $http.fetch(req2).then(function (res) {
           paurl = res.body.match(/ src="(.*?)'/)[1];
-          var paurl = "https://libvio.pro/vid/ty4.php?url=";
+          var paurl = "https://www.libvio.cc/vid/ty4.php?url=";
           var playAPIURL = paurl + url;
 
           var req = {
             url: playAPIURL,
             headers: {
-              Referer: "https://libvio.pro/",
+              Referer: "https://www.libvio.cc/",
             },
           };
 
@@ -177,7 +177,7 @@ function Player(inputURL) {
           var req = {
             url: playAPIURL,
             headers: {
-              Referer: "https://libvio.pro/",
+              Referer: "https://www.libvio.cc/",
             },
           };
 
@@ -208,13 +208,13 @@ function Player(inputURL) {
             paurl + url + "&next=" + next + "&id=" + id + "&nid=" + nid;
 
           if (!startsWithHttp(playAPIURL)) {
-            playAPIURL = "https://libvio.pro/" + playAPIURL;
+            playAPIURL = "https://www.libvio.cc/" + playAPIURL;
           }
 
           var req = {
             url: playAPIURL,
             headers: {
-              Referer: "https://libvio.pro/",
+              Referer: "https://www.libvio.cc/",
             },
           };
 
@@ -239,7 +239,7 @@ function gotoPlay(url) {
       headers: {
         "User-Agent":
           "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/18.1 Safari/605.1.15",
-        Referer: "https://libvio.pro/",
+        Referer: "https://www.libvio.cc/",
       },
     };
 
